a0d9d98cb27aa19450cdbb36d971f79f0d308b03
simplytrianglework-svg <simplytriangle.work@gmail.com>
2026-07-06 16:53:33 +0800

    preparation for site publishing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // or the workers.dev URL if you haven't attached a custom domain yet
+  // ...rest of your existing config
+  site: 'https://coffeebreak.simply-triangle.workers.dev',
+
+  integrations: [sitemap()]
+});
